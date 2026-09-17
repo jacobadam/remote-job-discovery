13b09463ccbca301046ea8ab24dee9680b25a3ea
jacobadam <jacobanevitt@gmail.com>
2026-09-17 14:51:13 +0100

    add interface IJobProvider

---
+++ backend/JobDiscovery.Api/Services/JobService/IJobProvider.cs
@@ -0,0 +1,10 @@
+using JobDiscovery.Api.Models.Jobs
+
+namespace JobDiscovery.Api.Services
+
+public interface IJobProvider
+{
+  Task<IReadOnlyList<JobListing>> GetRemoteJobsAsync(
+    CancellationToken cancellationToken = default
+  );
+}
